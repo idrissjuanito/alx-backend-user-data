dc39f3ad51b0b7583b506fdbb0688871655d3b78
idrissjuanito <idrissjuanito@gmail.com>
2024-04-16 10:04:59 +0000

    task 03 basic_auth: setting up auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Authentication module """
+from flask import request
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ enforces authorization on routes
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Gets authorization header for request
+        """
+        None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Gets current user making request
+        """
+        return None
