03d0f8c48b80b8f6819c61311b2ace5e99e90d1d
idrissjuanito <idrissjuanito@gmail.com>
2024-04-20 11:12:10 +0000

    decoding base64 encoded string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Auth module """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -14,3 +15,15 @@ class BasicAuth(Auth):
         if not ah.startswith("Basic"):
             return None
         return ah.split()[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Validates and decodes base64 authorization header """
+        ah = base64_authorization_header  # shorter var name
+        if ah is None or type(ah) is not str:
+            return None
+        try:
+            dcoded = base64.b64decode(ah)
+            return dcoded
+        except Exception:
+            return None
