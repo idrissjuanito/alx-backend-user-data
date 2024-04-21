5ba31063bd8a67a29bc13c5d56663465c5e334d6
idrissjuanito <idrissjuanito@gmail.com>
2024-04-21 12:19:38 +0000

    task corrections

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ Basic Auth module """
 from api.v1.auth.auth import Auth
+from typing import TypeVar
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -37,3 +39,21 @@ class BasicAuth(Auth):
         if ":" not in d_ah:
             return None, None
         return tuple(d_ah.split(":"))
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ returns the user object corresponding to email and password """
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        User.load_from_file()
+        users = User.search({"email": user_email, "password": user_pwd})
+        print(users)
+        if len(users) == 0:
+            return None
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            print(user)
+            return None
+        return user
