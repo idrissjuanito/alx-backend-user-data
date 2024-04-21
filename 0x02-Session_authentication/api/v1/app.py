9e951040ce4fe3abb3bfd767f99cca2a91719fbb
idrissjuanito <idrissjuanito@gmail.com>
2024-04-21 18:22:35 +0000

    catching requests with no cookies at before_request decorator

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,11 +35,14 @@ def before_requests():
     ex_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
     if not auth.require_auth(request.path, ex_paths):
         return
     if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.session_cookie(request) is None:
         abort(401)
     c_user = auth.current_user(request)
     if c_user is None:
