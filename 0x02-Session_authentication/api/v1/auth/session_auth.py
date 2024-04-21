462bfe3d9f9c857c005f004fad4a3c2d18dda855
idrissjuanito <idrissjuanito@gmail.com>
2024-04-21 17:30:45 +0000

    added session creation method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """ Session Auth Module """
+from uuid import uuid4
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """ Session auth class """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a new session """
+        if user_id is None or type(user_id) is not str:
+            return None
+        sess_id = str(uuid4())
+        self.user_id_by_session_id[sess_id] = user_id
+        return sess_id
