0f769629e08c73d62cf5742d09b8751670ced419
idrissjuanito <idrissjuanito@gmail.com>
2024-04-23 10:52:51 +0000

    Fixed typo in user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env  python3
-""" User module """
+#!/usr/bin/env python3
+""" User module and Base sqlalchemy init
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
